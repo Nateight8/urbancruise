b93c8963b954846d88a471cea1a808311f900668
Mbaocha Jonathan <mbaocha240793@gmail.com>
2025-04-22 15:52:58 +0100

    feat: update chat appbar to display participant data

--- frontend/web-app/src/app/messages/[messageid]/_components/chat-appbar.tsx
+++ frontend/web-app/src/app/messages/[messageid]/_components/chat-appbar.tsx
@@ -1,8 +1,23 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { IconChevronLeft } from "@tabler/icons-react";
+import { ConversationParticipant } from "@/graphql/operations/conversation-operations";
+import { useCachedUser } from "@/hooks/use-cached-user";
 
-export default function ChatAppBar() {
+export default function ChatAppBar({
+  participants,
+}: {
+  participants: ConversationParticipant[];
+}) {
+  const cachedUser = useCachedUser();
+
+  const otherParticipant = participants.find(
+    (participant) => participant.user.username !== cachedUser?.username
+  );
+
+  console.log("Other participant", otherParticipant);
+
   return (
     <header className="flex-shrink-0  h-16 flex  items-center">
       <div className="flex items-center gap-3">
@@ -16,10 +31,14 @@ export default function ChatAppBar() {
         </Button>
         <button className="flex items-center gap-2 hover:cursor-pointer hover:bg-accent/50 rounded-3xl p-1 pr-3 hover:border border border-transparent hover:border-border">
           <Avatar className="size-10">
-            <AvatarImage src="/images/pfp.jpeg" />
+            <AvatarImage
+              src={otherParticipant?.user.image ?? "/images/pfp/pfp.jpeg"}
+            />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-          <p className="text-base font-semibold">John Doe</p>
+          <p className="text-base font-semibold capitalize">
+            {otherParticipant?.user.username}
+          </p>
         </button>
       </div>
     </header>
