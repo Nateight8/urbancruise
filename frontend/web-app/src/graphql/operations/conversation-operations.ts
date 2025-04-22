6cca7cb62bafc610595054a203b37635de0944c2
Mbaocha Jonathan <mbaocha240793@gmail.com>
2025-04-22 15:47:12 +0100

    chore: update Conversation interface to include participants field

--- frontend/web-app/src/graphql/operations/conversation-operations.ts
+++ frontend/web-app/src/graphql/operations/conversation-operations.ts
@@ -1,7 +1,38 @@
 import { gql } from "@apollo/client";
 
 const conversationOperations = {
-  Querries: {},
+  Querries: {
+    getConversation: gql`
+      query Conversation($conversationId: ID!) {
+        conversation(id: $conversationId) {
+          createdAt
+          id
+          isDraft
+          isGroup
+          title
+          updatedAt
+          messages {
+            content
+            conversationId
+            id
+            isDeleted
+            isEdited
+            sender {
+              username
+              id
+            }
+            senderId
+          }
+          participants {
+            user {
+              image
+              username
+            }
+          }
+        }
+      }
+    `,
+  },
 
   Mutations: {
     sendMessage: gql`
@@ -13,5 +44,46 @@ const conversationOperations = {
     `,
   },
 };
+
+export interface Message {
+  content: string;
+  conversationId: string;
+  id: string;
+  isDeleted: boolean;
+  isEdited: boolean;
+  sender: {
+    username: string;
+    id: string;
+  };
+  senderId: string;
+}
+export interface Conversation {
+  id: string;
+  createdAt: Date;
+  isDraft: boolean;
+  isGroup: boolean;
+  title: string | null;
+  updatedAt: Date;
+  messages: Message[];
+  participants: ConversationParticipant[];
+}
+
+export interface ConversationParticipant {
+  conversationId: string;
+  user: {
+    id: string;
+    name: string | null;
+    username: string | null;
+    image: string | null;
+  };
+  lastMessage: {
+    content: string;
+  } | null;
+  lastMessageAt: Date | null;
+}
+
+export interface GetConversationResponse {
+  conversation: Conversation;
+}
 
 export default conversationOperations;
