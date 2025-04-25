713ce603105d8591bfba67a859fc6fd375fab075
Mbaocha Jonathan <mbaocha240793@gmail.com>
2025-04-25 09:43:45 +0100

    chore: remove duplicate js files from src directory

--- server/src/graphql/typeDefs/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Define the GraphQL schema
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_js_1 = require("./user.js");
-const conversation_js_1 = require("./conversation.js");
-const typeDefs = [user_js_1.userTypeDefs, conversation_js_1.conversationTypeDefs];
-exports.default = typeDefs;
